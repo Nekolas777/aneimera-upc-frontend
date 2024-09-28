c2b65cf0202c78752186f87b9a4a13004183fb2d
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-09-27 19:45:47 -0500

    feat: palet color for aneimera upc using tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      gray: {
+        DEFAULT: "#DFDFDF",
+      },
+      white: {
+        DEFAULT: "#FFFFFF",
+      },
+      red: {
+        DEFAULT: "#C42626",
+      },
+      title: {
+        DEFAULT: "#231F20",
+      },
+      parag: {
+        DEFAULT: "#251F21",
+      },
+    },
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
-}
-
+};
