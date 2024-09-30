e69a1e883261b770beb0a31d3d9a00ebd472520e
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-09-30 18:42:22 -0500

    update: footer and navbar added for screens

--- src/shared/components/NavigationBar.tsx
+++ src/shared/components/NavigationBar.tsx
@@ -1,9 +1,34 @@
+import { LogoutIcon } from "../../assets/icons/LogoutIcon";
+import { WhatsappIcon } from "../../assets/icons/WhatsappIcon";
+import LogoAneimera from "../../assets/images/aneimera-logo.png";
+
 export const NavigationBar = () => {
   return (
-    <header className="bg-red-600">
-      <div className="section-container">
-        <h1>NavigationBar</h1>
+    /* sticky top-0 z-50 considere this for topbar flexible */
+    <header className='bg-gray-200/80'>
+      <div className='section-container h-full flex items-center justify-between'>
+        <figure className='w-44 cursor-pointer'>
+          <img className='size-full' src={LogoAneimera} />
+        </figure>
+        <div className='flex flex-row gap-3 items-center'>
+          <div
+            className='flex flex-row gap-2 items-center border-r-2 
+            border-slate-900/80 pr-5 mr-3 py-1.5'
+          >
+            <WhatsappIcon />
+            <span>+ 51 972 829 123</span>
+          </div>
+          <button
+            className='flex flex-row gap-3 items-center bg-red-600 px-5 py-3 rounded-xl 
+            hover:bg-red-800 transition-all duration-300'
+          >
+            <span className='tracking-wide text-white'>
+              Cerrar sesión
+            </span>
+            <LogoutIcon />
+          </button>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
