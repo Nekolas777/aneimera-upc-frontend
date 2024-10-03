804ff03b0c22ba3c0c81f632188f782d25812dd0
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-10-02 22:33:16 -0500

    style: responsive design for events-information screen

--- src/features/events/components/ActionItem.tsx
+++ src/features/events/components/ActionItem.tsx
@@ -6,7 +6,7 @@ interface ActionItemProps {
 
 export const ActionItem = ({ caption }: ActionItemProps) => {
   return (
-    <li className='flex flex-row gap-3 items-center font-medium text-xl text-white/90'>
+    <li className='flex flex-row gap-3 items-center font-medium text-base md:text-xl text-white/90'>
       <CheckIcon />
       <span className='whitespace-nowrap'>{ caption }</span>
     </li>
--- src/shared/components/NavigationBar.tsx
+++ src/shared/components/NavigationBar.tsx
@@ -12,26 +12,33 @@ export const NavigationBar = () => {
     /* sticky top-0 z-50 consider this for topbar flexible in <header> */
     <header className='bg-gray-200/80'>
       <div className='section-container h-full flex items-center justify-between'>
-        <figure className='w-44 cursor-pointer'>
+        <figure className='w-32 sm:w-44 cursor-pointer'>
           <img className='size-full' src={LogoAneimera} />
         </figure>
         <div className='flex flex-row gap-3 items-center'>
           <a
             className='flex flex-row gap-2 items-center border-r-2 
-            border-slate-900/80 pr-5 mr-3 py-1.5 cursor-pointer group'
-            href="https://aneimera-upc.vercel.app/"
-            target="_blank"
-            rel="noopener noreferrer"
+            border-slate-900/80 pr-3 sm:pr-5 mr-1 sm:mr-3 py-1.5 cursor-pointer group'
+            href='https://aneimera-upc.vercel.app/'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <WebsiteIcon />
-            <span className="text-lg group-hover:underline">www.aneimeraupc.com.pe</span>
+            <span className='text-lg group-hover:underline hidden md:block'>
+              www.aneimeraupc.com.pe
+            </span>
           </a>
           <button
-            className='flex flex-row gap-3 items-center bg-red-600 px-5 py-3 rounded-xl 
+            className='flex flex-row gap-3 items-center bg-red-600 px-4 py-3 rounded-xl 
             hover:bg-red-800 transition-all duration-300'
             onClick={() => navigateTo("/")}
           >
-            <span className='tracking-wide text-white'>Cerrar sesión</span>
+            <span className='tracking-wide text-white whitespace-nowrap block md:hidden'>
+              Salir
+            </span>
+            <span className='tracking-wide text-white whitespace-nowrap hidden md:block'>
+              Cerrar Sesion
+            </span>
             <LogoutIcon />
           </button>
         </div>
