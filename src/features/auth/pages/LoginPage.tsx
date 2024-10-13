f8ecf2de4f108b298f33d1248680697df8d4d7bd
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2024-10-13 16:27:39 -0500

    Update LoginPage.tsx
    login update

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { MdVisibility } from "react-icons/md";
 
 import {
   RiMailLine,
@@ -17,63 +18,70 @@ export const LoginPage = () => {
     setShowPassword(!showPassword);
   }
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-[#8B8B8B]">
-      <div className="bg-white p-6 w-full md:w-96">
-        <section className="py-20">
-          <div className="flex items-center justify-center">
-            <img className="w-10 h-10" src="src/assets/logo.png" alt="imagen" />
-            <h1 className="flex items-center justify-center">ANEIMERA</h1>
-          </div>
 
-          <div className="mb-5">
-            <h1 className="text-2xl font-bold text-center">
-              Bienvenido a ANEIMERA
-            </h1>
-            <h5 className="text-1xl font-bold text-center">
-              Inicio de Sesión
-            </h5>
+
+
+
+
+
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      <div className="flex-1 flex flex-col justify-center items-center p-6 bg-white">
+        <div className="flex items-center mb-6">
+          <img className="w-10 h-10" src="src/assets/logo.png" alt="imagen" />
+          <h1 className="ml-2 text-xl">ANEIMERA</h1>
         </div>
+
+        <section className="py-4 text-center">
+          <h1 className="text-2xl font-bold">Bienvenido a ANEIMERA</h1>
+          <h5 className="text-lg font-semibold">Inicio de Sesión</h5>
         </section>
-        <form className="flex flex-col gap-4 ">
+
+        <form className="flex flex-col gap-4 w-full max-w-md">
           <div className="relative">
-            <RiMailLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
+            <RiMailLine className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
             <input
               type="text"
               className="w-full border border-gray-200 outline-none py-2 px-8 rounded-lg"
-              placeholder="Correo electrònico"
+              placeholder="Correo electrónico"
             />
           </div>
+
           <div className="relative">
-            <RiLockLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
+            <RiLockLine className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
             <input
-              type={showPassword ? "text" : "password"} /*si es verdadero muestra texto*/
+              type={showPassword ? "text" : "password" }
               className="w-full border border-gray-200 outline-none py-2 px-8 rounded-lg"
-              placeholder="password"
+              placeholder="Contraseña"
             />
             {showPassword ? (
               <RiEyeOffLine
                 onClick={handleShowPassword}
-                className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:cursor-pointer"
               />
             ) : (
               <RiEyeLine
                 onClick={handleShowPassword}
-                className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer"
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:cursor-pointer"
               />
             )}
           </div>
-          <div>         
-            <button className="mt-6 bg-[#CB4A4A] text-white w-full py-2 px-6 rounded-lg hover:scale-105 transition-all"> 
-              Sign IN
-            </button>
-          </div>
+
+          <button className="mt-3 bg-[#e80404] text-white w-full py-2 rounded-lg hover:scale-105 transition-all">
+            Iniciar Sesión
+          </button>
         </form>
       </div>
 
-      <section className="bg-[#8B8B8B] p-6 w-full md:w-96">
-        <img className="w-full h-full md:w-96" src="src/assets/logofondo.png" alt="imagen" />
+      <section className="hidden md:flex w-full md:w-1/2 bg-[#e80404]">
+        <img className="w-full h-auto max-h-1/2 object-contain" src="src/assets/logofondo.png" alt="imagen" />
       </section>
     </div>
-  )
+  );
+
+
+
+
+
+
 }
