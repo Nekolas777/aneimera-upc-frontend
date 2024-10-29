139c835728c3d74d7e3a81a9a9db4c401822f282
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-10-28 19:17:42 -0500

    Update: Added logout function to the useAuth context to remove the token from local storage when the user closes the session.

--- src/shared/components/NavigationBar.tsx
+++ src/shared/components/NavigationBar.tsx
@@ -2,22 +2,32 @@ import { useNavigate } from "react-router-dom";
 import { LogoutIcon } from "../../assets/icons/LogoutIcon";
 import LogoAneimera from "../../assets/images/aneimera-logo.png";
 import { WebsiteIcon } from "../../assets/icons/WebsiteIcon";
+import { useAuth } from "../../features/auth/hooks/useAuth";
 
 export const NavigationBar = () => {
+  const { logout } = useAuth();
   const navigate = useNavigate();
 
-  const navigateTo = (path: string) => navigate(path);
+  const signOut = () => {
+    logout();
+
+    // replace es para que no pueda retroceder en el hist de nav
+    // ya que una vez cerrado la sesion no podra ingresar a la ruta anterior
+    navigate("/login", {
+      replace: true
+    });
+  };
 
   return (
     /* sticky top-0 z-50 consider this for topbar flexible in <header> */
-    <header className='bg-gray-200/80'>
+    <header className='sticky top-0 z-50 bg-gray-200'>
       <div className='section-container h-full flex items-center justify-between'>
         <figure className='w-32 sm:w-44 cursor-pointer'>
           <img className='size-full' src={LogoAneimera} />
         </figure>
         <div className='flex flex-row gap-3 items-center'>
           <a
-            className='flex flex-row gap-2 items-center border-r-2 
+            className='flex flex-row gap-3 items-center border-r-2 
             border-slate-900/80 pr-3 sm:pr-5 mr-1 sm:mr-3 py-1.5 cursor-pointer group'
             href='https://aneimera-upc.vercel.app/'
             target='_blank'
@@ -31,7 +41,7 @@ export const NavigationBar = () => {
           <button
             className='flex flex-row gap-3 items-center bg-red-600 px-4 py-3 rounded-xl 
             hover:bg-red-800 transition-all duration-300'
-            onClick={() => navigateTo("/")}
+            onClick={() => signOut()}
           >
             <span className='tracking-wide text-white whitespace-nowrap block md:hidden'>
               Salir
