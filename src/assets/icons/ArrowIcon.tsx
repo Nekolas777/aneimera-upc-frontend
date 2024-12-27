83dda664d77dfa659e6dad6a7f53159f5dd51920
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-12-27 16:09:00 -0500

    feat: add icons across the app

--- src/assets/icons/ArrowIcon.tsx
+++ src/assets/icons/ArrowIcon.tsx
@@ -1,14 +1,14 @@
 interface ArrowIconProps {
-  direction?: 'left' | 'right';
+  direction?: "left" | "right";
 }
 
-export const ArrowIcon = ({ direction = 'left' }: ArrowIconProps) => {
+export const ArrowIcon = ({ direction = "left" }: ArrowIconProps) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 320 512'
-      className={`size-3 ${direction === 'right' ? 'rotate-180' : ''}`}
-      fill="#000"
+      className={`size-3 ${direction === "right" ? "rotate-180" : ""}`}
+      fill='#000'
     >
       <path d='M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z' />
     </svg>
