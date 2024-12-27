ae6d52e994d240e94a4dc9d012e3af6617dcc978
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-12-27 16:11:52 -0500

    feat: technical visit preview dialog for update and create visit

---
+++ src/features/events/components/TechnicalVisitPreviewDialog.tsx
@@ -0,0 +1,80 @@
+import { CalendarIcon } from "../../../assets/icons/CalendarIcon";
+import { CloseIcon } from "../../../assets/icons/CloseIcon";
+import { Visita } from "../model/visita";
+
+interface TechnicalVisitPreviewDialogProps {
+  visit: Visita;
+  file?: File | null;
+  onClose: () => void;
+}
+
+export const TechnicalVisitPreviewDialog = ({ visit, file, onClose }: TechnicalVisitPreviewDialogProps) => {
+  const getImageSrc = (rutaImagen: string, file?: File | null, ) => {
+    if (file) {
+      return URL.createObjectURL(file);
+    }
+    return `https://aneimeraupc.somee.com${rutaImagen}`;
+  };
+
+  return (
+    <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
+      <div className='relative p-8 bg-white w-11/12 max-w-[800px]'>
+        <button
+          className='absolute top-0 right-0 bg-red-500 hover:bg-red-600 transition-all duration-200 ease-linear p-2'
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </button>
+        <article className='w-full flex flex-col bg-slate-200'>
+          <div>
+            <img
+              className='w-full h-[22rem] object-cover sm:object-fill'
+              src={getImageSrc(visit.rutaImagen!, file)}
+              alt={`Imagen de la visita técnica ${visit.titulo}`}
+            />
+          </div>
+          <div className='flex flex-col py-6 px-5'>
+            <div className='flex flex-col gap-5'>
+              <div className='flex flex-row justify-between items-center gap-5'>
+                <h2 className='font-medium workshop-title'>{visit.titulo}</h2>
+                <p className='text-sm'>
+                  <span className='font-medium mr-1.5' style={{ fontSize: '17px' }}>Fecha:</span>
+                  {visit.fecha}
+                </p>
+              </div>
+              <p
+                className='description font-normal text-sm md:text-base py-1 tracking-wide text-wrap
+                    leading-relaxed text-slate-900 break-words'
+                style={{ lineHeight: '1.65' }}
+              >
+                {visit.descripcion}
+              </p>
+              <div className='flex flex-row justify-between items-center'>
+                <div className='flex flex-row gap-3'>
+                  <a
+                    href={visit.enlace}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    aria-label='Ir a la pagina de Facebook de Aneimera UPC'
+                    className='px-4 py-2 bg-red-700 text-white text-lg mt-2 self-center sm:self-start
+                    hover:bg-red-800 transition-all duration-200 ease'
+                  >
+                    Más información
+                  </a>
+                </div>
+                <div
+                  className='calendar-btn cursor-pointer bg-[#3C3737] p-2 rounded-full
+                    hover:bg-black transition-all duration-200 ease-in'
+                  data-visit-date={visit.fecha}
+                  data-visit-title={visit.titulo}
+                >
+                  <CalendarIcon />
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
+};
